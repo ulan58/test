332ee48fa08423d84ee293f1028c1f0874abc95d
murat <ulan58>
2021-07-08 11:51:39 +0600

    initial commit.

---
+++ git_commands.py
@@ -0,0 +1,13 @@
+git init - .git 
+git remote add name_url
+git pull origin master
+git status
+git add 
+git commit -m 'comments'
+git branch
+git branch name_branch
+git checkout name_branch
+git push name_branch
+git push origin master
+git reset filename
+
